19fb06c01f25129854e03287493c4f76655694bf
Cai Yuanqing <ckjacket@gmail.com>
2014-07-09 15:16:34 +0000

    one more

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
+
   response.send(fs.readFileSync("index.html").toString('utf-8'))
 })
 
